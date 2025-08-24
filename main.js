98a6c9d31e9040b103a6dd105c2781d1d0a0c37c
thalyson123ui <69772569+thalyson123ui@users.noreply.github.com>
2025-08-24 09:30:16 -0300

    atualização css e js

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//Mobile Navbar 
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+for(let i = 0; i < navElemArr.length; i++) {
+    navElemArr[i].addEventListener('click', function() {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    })
+}
+
+//Active Header when scrolling
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener('scroll', function() {
+    window.scrollY >= 200 ? header.classList.add("active") : header.classList.remove("active");
+})
